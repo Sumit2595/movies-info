bdd6687793581cfa3f03b095a1ccc395d97c3a51
Sumit2595 <sc1645@srmist.edu.in>
2021-05-24 14:06:18 +0530

    Updates

---
+++ precache-manifest.961b571f368cbc234bc2a8311a8e20c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f99fbf46eccc757812958560f74f28e",
+    "url": "/movies-info/index.html"
+  },
+  {
+    "revision": "4e61bbeeccfcfc4c5d73",
+    "url": "/movies-info/static/css/main.1f5697f9.chunk.css"
+  },
+  {
+    "revision": "fe4dec88b4311d4cc60d",
+    "url": "/movies-info/static/js/2.fd08b29f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movies-info/static/js/2.fd08b29f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e61bbeeccfcfc4c5d73",
+    "url": "/movies-info/static/js/main.ee457335.chunk.js"
+  },
+  {
+    "revision": "4fcc3e18561caabbcedd",
+    "url": "/movies-info/static/js/runtime-main.d9b49b9a.js"
+  }
+]);
